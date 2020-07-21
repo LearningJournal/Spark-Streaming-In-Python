d707b1d116d48e60132d836468216e305da82c5b
Learning Journal <pandey.kumar.prashant@gmail.com>
2020-07-21 21:28:01 +0530

    Initial Commit

---
+++ 01-StreamingWC/StreamingWC.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+from lib.logger import Log4j
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("Streaming Word Count") \
+        .master("local[3]") \
+        .getOrCreate()
+
+    logger = Log4j(spark)
+
+    lines_df = spark.readStream \
+        .format("socket") \
+        .option("host", "localhost") \
+        .option("port", "9999") \
+        .load()
+
+    lines_df.printSchema()
+
+    # words_df = lines_df.select(explode(split("value", " ")).alias("word"))
+    words_df = lines_df.select(expr("explode(split(value,' ')) as word"))
+    counts_df = words_df.groupBy("word").count()
+
+    word_count_query = counts_df.writeStream \
+        .format("console") \
+        .outputMode("complete") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .start()
+
+    logger.info("Listening to localhost:9999")
+    word_count_query.awaitTermination()
