4e3c48564fea5ddf770e96ef53ce04c8b3ff9401
Learning Journal <pandey.kumar.prashant@gmail.com>
2020-07-26 14:29:58 +0530

    Initial Commit

---
+++ 02-FileStreamDemo/FileStreamDemo.py
@@ -0,0 +1,79 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, IntegerType, ArrayType, \
+    DateType
+
+from lib.logger import Log4j
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("File Streaming Demo") \
+        .master("local[3]") \
+        .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .getOrCreate()
+
+    logger = Log4j(spark)
+
+    schema = StructType([
+        StructField("InvoiceNumber", StringType()),
+        StructField("CreatedTime", LongType()),
+        StructField("StoreID", StringType()),
+        StructField("PosID", StringType()),
+        StructField("CashierID", StringType()),
+        StructField("CustomerType", StringType()),
+        StructField("CustomerCardNo", StringType()),
+        StructField("TotalAmount", DoubleType()),
+        StructField("NumberOfItems", IntegerType()),
+        StructField("PaymentMethod", StringType()),
+        StructField("CGST", DoubleType()),
+        StructField("SGST", DoubleType()),
+        StructField("CESS", DoubleType()),
+        StructField("DeliveryType", StringType()),
+        StructField("DeliveryAddress", StructType([
+            StructField("AddressLine", StringType()),
+            StructField("City", StringType()),
+            StructField("State", StringType()),
+            StructField("PinCode", StringType()),
+            StructField("ContactNumber", StringType())
+        ])),
+        StructField("InvoiceLineItems", ArrayType(StructType([
+            StructField("ItemCode", StringType()),
+            StructField("ItemDescription", StringType()),
+            StructField("ItemPrice", DoubleType()),
+            StructField("ItemQty", IntegerType()),
+            StructField("TotalValue", DoubleType())
+        ]))),
+    ])
+
+    raw_df = spark.readStream \
+        .format("json") \
+        .schema(schema) \
+        .option("path", "input") \
+        .option("maxFilesPerTrigger", 1) \
+        .load()
+
+    explode_df = raw_df.selectExpr("InvoiceNumber", "CreatedTime", "StoreID", "PosID",
+                                   "CustomerType", "PaymentMethod", "DeliveryType", "DeliveryAddress.City",
+                                   "DeliveryAddress.State",
+                                   "DeliveryAddress.PinCode", "explode(InvoiceLineItems) as LineItem")
+
+    flattened_df = explode_df \
+        .withColumn("ItemCode", expr("LineItem.ItemCode")) \
+        .withColumn("ItemDescription", expr("LineItem.ItemDescription")) \
+        .withColumn("ItemPrice", expr("LineItem.ItemPrice")) \
+        .withColumn("ItemQty", expr("LineItem.ItemQty")) \
+        .withColumn("TotalValue", expr("LineItem.TotalValue")) \
+        .drop("LineItem")
+
+    invoiceWriterQuery = flattened_df.writeStream \
+        .format("json") \
+        .queryName("Flattened Invoice Writer") \
+        .outputMode("append") \
+        .option("path", "output") \
+        .option("checkpointLocation", "chk-point-dir") \
+        .trigger(processingTime="1 minute") \
+        .start()
+
+    logger.info("Flattened Invoice Writer started")
+    invoiceWriterQuery.awaitTermination()
