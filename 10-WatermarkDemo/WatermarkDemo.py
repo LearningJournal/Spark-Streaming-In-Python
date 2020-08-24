e1b41d28638d6b02d385a2a4e6b6d92e0186a98e
Prashant Kumar Pandey <LearningJournal@users.noreply.github.com>
2020-08-24 14:47:20 +0530

    Initial Commit

--- 10-WatermarkDemo/WatermarkDemo.py
+++ 10-WatermarkDemo/WatermarkDemo.py
@@ -1,6 +1,6 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, to_timestamp, window
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import from_json, col, to_timestamp, window, expr, sum
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 from lib.logger import Log4j
 
@@ -10,48 +10,46 @@ if __name__ == "__main__":
         .appName("Watermark Demo") \
         .master("local[3]") \
         .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("spark.sql.shuffle.partitions", 2) \
         .getOrCreate()
 
     logger = Log4j(spark)
 
-    invoice_schema = StructType([
-        StructField("InvoiceNumber", StringType()),
+    stock_schema = StructType([
         StructField("CreatedTime", StringType()),
-        StructField("StoreID", StringType()),
-        StructField("TotalAmount", DoubleType())
+        StructField("Type", StringType()),
+        StructField("Amount", IntegerType()),
+        StructField("BrokerCode", StringType())
     ])
 
     kafka_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("subscribe", "invoices") \
+        .option("subscribe", "trades") \
         .option("startingOffsets", "earliest") \
         .load()
 
-    value_df = kafka_df.select(from_json(col("value").cast("string"), invoice_schema).alias("value"))
+    value_df = kafka_df.select(from_json(col("value").cast("string"), stock_schema).alias("value"))
 
-    # value_df.printSchema()
-    # value_df.show(truncate=False)
+    trade_df = value_df.select("value.*") \
+        .withColumn("CreatedTime", to_timestamp(col("CreatedTime"), "yyyy-MM-dd HH:mm:ss")) \
+        .withColumn("Buy", expr("case when Type == 'BUY' then Amount else 0 end")) \
+        .withColumn("Sell", expr("case when Type == 'SELL' then Amount else 0 end"))
 
-    invoice_df = value_df.select("value.*") \
-        .withColumn("CreatedTime", to_timestamp("CreatedTime", "yyyy-MM-dd HH:mm:ss"))
+    window_agg_df = trade_df \
+        .withWatermark("CreatedTime", "30 minute") \
+        .groupBy(window(col("CreatedTime"), "15 minute")) \
+        .agg(sum("Buy").alias("TotalBuy"),
+             sum("Sell").alias("TotalSell"))
 
-    count_df = invoice_df \
-        .withWatermark("CreatedTime", "5 minute") \
-        .groupBy("StoreID",
-                 window("CreatedTime", "5 minute", "5 minute")) \
-        .count()
+    output_df = window_agg_df.select("window.start", "window.end", "TotalBuy", "TotalSell")
 
-    # count_df.printSchema()
-    # count_df.show(truncate=False)
-
-    output_df = count_df.select("StoreID", "window.start", "window.end", "count")
-
-    windowQuery = output_df.writeStream \
+    window_query = output_df.writeStream \
         .format("console") \
         .outputMode("update") \
         .option("checkpointLocation", "chk-point-dir") \
+        .trigger(processingTime="30 second") \
         .start()
 
-    logger.info("Counting Invoices")
-    windowQuery.awaitTermination()
+    logger.info("Waiting for Query")
+    window_query.awaitTermination()
